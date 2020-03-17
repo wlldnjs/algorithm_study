3e940424f5ce177f4735287c1f8a2c273877e5ca
wlldnjs <wlldnjs@naver.com>
2020-03-17 21:27:50 +0900

    2.1 Task Score: 100%
    Correctness: 100%

--- Codility/2.1CyclicRotation.kt
+++ Codility/2.1CyclicRotation.kt
@@ -1,15 +1,16 @@
 /**
     https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
-    Task Score: 87%(Correctness: 87%)
+    Task Score: 100%(Correctness: 100%)
 */
 fun solution(A: IntArray, K: Int): IntArray {
-    val arraySize = A.size
-    val rotationCount = K % arraySize
-    var returnArray = IntArray(arraySize)
-    
-    for(i in A.indices){
-        returnArray[(i+rotationCount) % arraySize] = A[i]
+    if(A.size != 0){
+        val rotationCount = K % A.size
+        var returnArray = IntArray(A.size)
+        for(i in A.indices){
+            returnArray[(i+rotationCount) % A.size] = A[i]
+        }
+        return returnArray
+    } else {
+        return A
     }
-    
-    return returnArray
 }
