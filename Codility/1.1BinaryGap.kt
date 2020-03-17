a48dd31302d91dcbc75faddd36fb765c49abb4e2
wlldnjs <wlldnjs@naver.com>
2020-03-17 20:11:55 +0900

    1.1 Task Score 100%

--- Codility/1.1BinaryGap.kt
+++ Codility/1.1BinaryGap.kt
@@ -1,1 +1,23 @@
- 
+ /**
+    https://app.codility.com/programmers/lessons/1-iterations/binary_gap/start/
+    Task Score: 100%(Correctness: 100%)
+*/
+
+fun solution(N: Int): Int {
+    val binaryString = Integer.toBinaryString(N)
+    var maxBinaryGap = 0
+    var binaryGap = 0
+    
+    for(idx in binaryString.indices){
+        if(binaryString[idx] == '1'){
+            if(binaryGap > 0 && binaryGap > maxBinaryGap){
+                maxBinaryGap = binaryGap
+            }
+            binaryGap = 0
+        } else { 
+            binaryGap++
+        }
+    }
+    
+    return maxBinaryGap
+}
