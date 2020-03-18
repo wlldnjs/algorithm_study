700c79a155eba028a40303c274357f676682f071
wlldnjs <wlldnjs@naver.com>
2020-03-18 09:58:04 +0900

    2.2 Task Score: 77%
    Correctness: 100%, Performance: 50%

--- Codility/2.2OddOccurrencesInArray.kt
+++ Codility/2.2OddOccurrencesInArray.kt
@@ -1,17 +1,15 @@
 /**
     https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
-    Task Score: 66%(Correctness: 100%, Performance: 25%)
+    Task Score: 77%(Correctness: 100%, Performance: 50%)
 */
 
 fun solution(A: IntArray): Int {
-    val list = A.toMutableList()
-    list.sort()
-    
-    val lastCount = list.size.rem(2)
-    for(i in 0 until list.size - lastCount step 2){
-        if(list[i] != list[i+1]){
-            return list[i]
+    A.sort()
+    val lastIndex = A.size % 2
+    for(i in 0 until (A.size - lastIndex) step 2){
+        if(A[i] != A[i+1]){
+            return A[i]
         }
     }
-    return list[list.size -1]
+    return A[A.size - 1]
 }
