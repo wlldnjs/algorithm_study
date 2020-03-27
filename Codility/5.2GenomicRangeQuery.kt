74d24fe9361638ec3fead609a58ea65353824e1c
wlldnjs <wlldnjs@naver.com>
2020-03-27 10:46:05 +0900

    5.2 TaskScore: 100%
    Correctness: 100%, Performance: 100%

--- Codility/5.2GenomicRangeQuery.kt
+++ Codility/5.2GenomicRangeQuery.kt
@@ -1,18 +1,36 @@
 /**
     https://app.codility.com/programmers/lessons/5-prefix_sums/genomic_range_query/
-    Task Score: 62%(Correctness: 100%, Performance: 0%)
+    Task Score: 100%(Correctness: 100%, Performance: 100%)
 */
-fun solution(inputText: String, startArray: IntArray, endArray: IntArray): IntArray {
-    val returnArray = IntArray(startArray.size)
-    for(i in returnArray.indices){
-        returnArray[i] = inputText.substring(startArray[i],endArray[i]+1).min()?.let{
-            when(it){
-                'A' -> 1
-                'C' -> 2
-                'G' -> 3
-                else -> 4
-            }
-        }!!
+fun solution(inputString: String, p: IntArray, q: IntArray): IntArray {
+    val stringToIntArray = Array(inputString.length+1, { IntArray(4) })
+    
+    val counter = IntArray(4)
+    for (i in inputString.indices) {
+        when (inputString[i]) {
+            'A' -> counter[0]++
+            'C' -> counter[1]++
+            'G' -> counter[2]++
+            'T' -> counter[3]++
+        }
+        for (j in counter.indices) {
+            stringToIntArray[i+1][j] = counter[j]
+        }
+
     }
+    
+    var returnArray = IntArray(p.size)
+    for(i in p.indices){
+        for(j in counter.indices){
+            counter[j] = stringToIntArray[q[i]+1][j] - stringToIntArray[p[i]][j]
+        }
+        for(j in counter.indices){
+            if(counter[j] > 0){
+                returnArray[i] = j+1
+                break
+            }
+        }
+    }
+
     return returnArray
 }
