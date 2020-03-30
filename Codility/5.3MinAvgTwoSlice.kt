480f7a948573332a4476f4c87551d56faea099a5
wlldnjs <wlldnjs@naver.com>
2020-03-30 21:34:04 +0900

    5.3 Task Score: 60%
    Correctness: 100%, Performance: 20%

--- Codility/5.3MinAvgTwoSlice.kt
+++ Codility/5.3MinAvgTwoSlice.kt
@@ -1,32 +1,23 @@
 /**
     https://app.codility.com/programmers/lessons/5-prefix_sums/min_avg_two_slice/
-    Task Score: 30%(Correctness: 40%, Performance: 20%)
+    Task Score: 60%(Correctness: 100%, Performance: 20%)
 */
-
-fun solution(inputArray: IntArray): Int {
-    
-    var minIndex = 0
+fun solution(A: IntArray): Int {
     var minValue = Float.MAX_VALUE
-    
-    for(i in inputArray.indices){
-        
-        val startValue = inputArray[i]
-        var tempValue: Float = startValue.toFloat()
-        var divideValue = 2
-        for(j in i+1 .. inputArray.size-1){
-            if(inputArray[j] <= startValue){
-                val temp:Float =  (tempValue + inputArray[j]) / divideValue
-                divideValue++
-                if(temp < minValue){
-                    minValue = temp
-                    minIndex = i
-                }
-            } else {
-                if(j == i+1){
-                    break
-                }
+    var minIndex = -1
+
+    for(i in A.indices){
+        var sumValue: Float = A[i].toFloat()
+        var count = 1
+        for(j in i+1 until A.size){
+            sumValue += A[j]
+            val averageValue = sumValue / ++count
+            if(averageValue < minValue){
+                minValue = averageValue
+                minIndex = i
             }
         }
     }
+    
     return minIndex
 }
