064768e3c01a4e2d666275cdeed10a2487fd98b0
wlldnjs <wlldnjs@naver.com>
2020-04-01 10:24:14 +0900

    6.4 Task Score: 93%
    Correctness: 90%, Performance: 100%

---
+++ Codility/6.4Triangle.kt
@@ -0,0 +1,20 @@
+/**
+    https://app.codility.com/programmers/lessons/6-sorting/triangle/
+    Task Score: 93%(Correctness: 90%, Performance: 100%)
+*/
+fun solution(inputArray: IntArray): Int {
+    val isExist = 1
+    val isNotExist = 0
+    
+    if(inputArray.size < 3){
+        return isNotExist
+    } else {
+        inputArray.sort()
+        for(i in inputArray.size-1 downTo 2 step 1){
+            if(inputArray[i] < inputArray[i-1].plus(inputArray[i-2])){
+                return isExist
+            }
+        }
+        return isNotExist
+    }
+}
