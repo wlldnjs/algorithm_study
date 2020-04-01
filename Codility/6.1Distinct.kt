f23310b0467c7c0179b3ec4eb31e10e5558d78f5
wlldnjs <wlldnjs@naver.com>
2020-04-01 09:42:56 +0900

    6.1 Task Score: 100%
    Correctness: 100%, Performance: 100%

---
+++ Codility/6.1Distinct.kt
@@ -0,0 +1,7 @@
+/**
+    https://app.codility.com/programmers/lessons/6-sorting/distinct/
+    Task Score: 100%(Correctness: 100%, Performance: 100%)
+*/
+fun solution(inputArray: IntArray): Int {
+    return inputArray.toHashSet().size
+}
