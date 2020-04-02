eabd6cdc7e3040941949b5e9351c1c68d44b18d0
wlldnjs <wlldnjs@naver.com>
2020-04-02 20:06:42 +0900

    6.3 Task Score: 81%
    Correctness: 100%, Performance: 62%

--- Codility/6.3NumberOfDiscIntersections.kt
+++ Codility/6.3NumberOfDiscIntersections.kt
@@ -1,25 +1,23 @@
 /**
     https://app.codility.com/programmers/lessons/6-sorting/number_of_disc_intersections/
-    Task Score: 75%(Correctness: 87%, Performance: 62%)
+    Task Score: 81%(Correctness: 100%, Performance: 62%)
 */
 fun solution(inputArray: IntArray): Int {
-    val leftPointArray = IntArray(inputArray.size)
-    val rightPointArray = IntArray(inputArray.size)
-    
-    var intersectionCount = 0
+    val pointArray = Array(inputArray.size){IntArray(2)}
+    var intersection = 0
     
     for(i in inputArray.indices){
-        leftPointArray[i] = if(i.minus(inputArray[i]) > 0){i.minus(inputArray[i])} else {0}
-        rightPointArray[i] = if(i.plus(inputArray[i]) > Integer.MAX_VALUE){Integer.MAX_VALUE} else {i.plus(inputArray[i])}
+        pointArray[i][0] = if(i.minus(inputArray[i]) < 0) 0 else {i.minus(inputArray[i])}
+        pointArray[i][1] = if(Integer.MAX_VALUE.minus(i).minus(inputArray[i]) < 0) Integer.MAX_VALUE else i.plus(inputArray[i])
     }
     
     for(i in inputArray.indices){
         for(j in i+1 until inputArray.size){
-            if(rightPointArray[i] >= leftPointArray[j]){
-                intersectionCount++
+            if(pointArray[i][1] >= pointArray[j][0]){
+                intersection++
             }
         }
     }
     
-    return intersectionCount
+    return intersection
 }
