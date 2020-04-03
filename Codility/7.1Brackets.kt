cd0e338f4303c05aaddce4d39339745365c406da
wlldnjs <wlldnjs@naver.com>
2020-04-03 09:14:01 +0900

    7.1 Task Score: 100%
    Correctness: 100%, Performance: 100%

--- Codility/7.1Brackets.kt
+++ Codility/7.1Brackets.kt
@@ -1,20 +1,34 @@
 /**
     https://app.codility.com/programmers/lessons/7-stacks_and_queues/brackets/
-    Task Score: 37%(Correctness: 33%, Performance: 40%)
+    Task Score: 100%(Correctness: 100%, Performance: 100%)
 */
 import java.util.*
+
 fun solution(inputString: String): Int {
-    val bracket = Stack<Char>()
+    val smallBracket = 1
+    val mediumBracket = 2
+    val largeBracket = 3
+    
+    val stack = Stack<Int>()
+    
+    fun checkPeek(bracket: Int): Boolean{
+        if(stack.isNotEmpty() && stack.peek() == bracket){
+            stack.pop()
+            return true
+        }
+        return false
+    }
     
     for(i in inputString.indices){
         when(inputString[i]){
-            '(' -> bracket.push(inputString[i])
-            ')' -> if(bracket.peek() == '('){bracket.pop()} else {return 0}
-            '{' -> bracket.push(inputString[i])
-            '}' -> if(bracket.peek() == '{'){bracket.pop()} else {return 0}
-            '[' -> bracket.push(inputString[i])
-            ']' -> if(bracket.peek() == '['){bracket.pop()} else {return 0}
+            '(' -> stack.push(smallBracket)
+            '{' -> stack.push(mediumBracket)
+            '[' -> stack.push(largeBracket)
+            ')' -> if(!checkPeek(smallBracket)) return 0
+            '}' -> if(!checkPeek(mediumBracket)) return 0
+            ']' -> if(!checkPeek(largeBracket)) return 0
         }
     }
-    return if(bracket.isEmpty()) 1 else 0
+    
+    return if(stack.isEmpty()) 1 else 0
 }
