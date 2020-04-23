066b2d8b487bdbd62c54da5139e0df43d9373c78
wlldnjs <wlldnjs@naver.com>
2020-04-23 11:17:02 +0900

    합계: 100.0 / 100.0
    정확성: 100.0

--- Programmers/크레인 인형뽑기 게임.kt
+++ Programmers/크레인 인형뽑기 게임.kt
@@ -1,41 +1,39 @@
 /**
     https://programmers.co.kr/learn/courses/30/lessons/64061?language=kotlin
-    합계: 90.9 / 100.0(정확성: 90.9)
+    합계: 100.0 / 100.0(정확성: 100.0)
 */
-
 import java.util.*
 
 class Solution {
     fun solution(board: Array<IntArray>, moves: IntArray): Int {
-        val maxIndex = board.size-1
-        
+        val topPositions = IntArray(board.size)
         val basketStack = Stack<Int>()
         var popCount = 0
         
-        val topPositions = IntArray(board.size)
-        for(i in board.indices){        
+        for(i in board.indices){    // topPositions 초기화
             for(j in board.indices){
                 if(board[j][i] > 0){
                     topPositions[i] = j
                     break
                 }
-                if(j == board.size-1){  // 인형이 없으면
-                    topPositions[i] = board.size-1
+                if(j == board.size-1){
+                    topPositions[i] = board.size
                 }
             }
         }
         
         for(i in moves.indices){
-            if(!basketStack.empty() && basketStack.peek() == board[topPositions[moves[i]-1]][moves[i]-1]){
-                basketStack.pop()
-                popCount = popCount + 2
-            } else{
-                basketStack.push(board[topPositions[moves[i]-1]][moves[i]-1])
-            }
-            if(topPositions[moves[i]-1] < board.size-1){
+            if(topPositions[moves[i]-1] < board.size){    // 크레인이 위치한 곳에 인형이 있을 때
+                if(!basketStack.isEmpty() && basketStack.peek() == board[topPositions[moves[i]-1]][moves[i]-1]){    // 바구니에 같은 인형이 있다면
+                    basketStack.pop()
+                    popCount = popCount+2
+                } else{
+                    basketStack.push(board[topPositions[moves[i]-1]][moves[i]-1])
+                }
                 topPositions[moves[i]-1]++
             }
-        }    
+        }
+        
         return popCount
     }
 }
