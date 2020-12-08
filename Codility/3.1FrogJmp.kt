d02e8bf50b3d885b0ee2a60f7acb8885561507e3
wlldnjs <wlldnjs@naver.com>
2020-12-08 15:16:51 +0900

    Update 3.1FrogJmp.kt

--- Codility/3.1FrogJmp.kt
+++ Codility/3.1FrogJmp.kt
@@ -6,3 +6,14 @@ fun solution(X: Int, Y: Int, D: Int): Int {
     val distance = Y.minus(X)
     return if(distance.rem(D) != 0){distance.div(D).plus(1)} else{distance.div(D)}
 }
+
+// 가독성 향상
+fun solution2(startingPoint: Int, destination: Int, jumpLength: Int): Int {
+    val goal = destination - startingPoint
+    var jumpCount = goal / jumpLength
+    if(goal % jumpLength > 0){
+        jumpCount++
+    }
+    
+    return jumpCount
+}
