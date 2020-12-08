5bc01b81ecb7e073ef4db9f8ea69c247503a6090
wlldnjs <wlldnjs@naver.com>
2020-12-08 14:05:05 +0900

    Update 2.1CyclicRotation.kt

--- Codility/2.1CyclicRotation.kt
+++ Codility/2.1CyclicRotation.kt
@@ -16,11 +16,13 @@ fun solution(A: IntArray, K: Int): IntArray {
 }
 
 fun solution2(array: IntArray, K: Int): IntArray {
-    val arraySize = A.size
-    val rotateNum = K % arraySize
+    val arraySize = array.size
+    val rotateNum = if (arraySize > 0) {
+        K % arraySize
+    } else K
 
     val returnArray = IntArray(arraySize)
-    array.forEachIndexed{index, item ->
+    array.forEachIndexed { index, item ->
         returnArray[(index + rotateNum) % arraySize] = item
     }
 
