27d496d0824f967bea2679400471c68e4f3ef929
wlldnjs <wlldnjs@naver.com>
2022-10-31 18:06:29 +0900

    정렬 알고리즘 구현2
    퀵 정렬, 병합 정렬

--- ios/sort_algoritm.swift
+++ ios/sort_algoritm.swift
@@ -34,3 +34,42 @@ func insertionSort(_ array: inout [Int]){
         }
     }
 }
+
+func quickSort(_ array: [Int]) -> [Int]{
+    guard let first = array.first, array.count > 1 else{ return array }
+    
+    let pivot = first
+    let left = array.filter{$0 < pivot}
+    let right = array.filter{$0 > pivot}
+    
+    return quickSort(left) + [pivot] + quickSort(right)
+}
+
+func mergeSort(_ array: [Int]) -> [Int] {
+    if array.count <= 1 {return array}
+    let center = array.count / 2
+    let left = Array(array[0..<center])
+    let right = Array(array[center..<array.count])
+    
+    func merge(_ left: [Int], _ right: [Int]) -> [Int]{
+        var left = left
+        var right = right
+        var result: [Int] = []
+        
+        while(!left.isEmpty && !right.isEmpty){
+            if(left[0] < right[0]){
+                result.append(left.removeFirst())
+            } else {
+                result.append(right.removeFirst())
+            }
+        }
+        if(!left.isEmpty) {
+            result.append(contentsOf: left)
+        }
+        if(!right.isEmpty) {
+            result.append(contentsOf: right)
+        }
+        return result
+    }
+    return merge(mergeSort(left), mergeSort(right))
+}
